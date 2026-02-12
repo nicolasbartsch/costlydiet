0959b049525ad0b89320f95555e6e4d8c0cf47ba
alice <alice@email>
2026-02-12 17:52:03 +0100

    chore(service): integrate ingredient repository and mapper into service layer
    Lets the ingredient service communicate with the mapper class and
    persistance layer to query data.
    
    Refs: #31

--- backend/src/main/java/com/bartsch/costlydiet/service/implementation/IngredientServiceImpl.java
+++ backend/src/main/java/com/bartsch/costlydiet/service/implementation/IngredientServiceImpl.java
@@ -1,12 +1,23 @@
 package com.bartsch.costlydiet.service.implementation;
 
 import com.bartsch.costlydiet.service.IngredientService;
+import com.bartsch.costlydiet.model.mapper.IngredientMapper;
+import com.bartsch.costlydiet.repository.IngredientRepository;
 
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class IngredientServiceImpl implements IngredientService {
+
+  private final IngredientRepository ingredientRepository;
+  private final IngredientMapper ingredientMapper;
+
+  public IngredientServiceImpl(IngredientRepository ingredientRepository, IngredientMapper ingredientMapper) {
+    this.ingredientRepository = ingredientRepository;
+    this.ingredientMapper = ingredientMapper;
+  }
 
 }
