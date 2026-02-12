b4fce06b98777c6aa6db0d689fc425e8e532baf5
alice <alice@email>
2026-02-12 17:50:29 +0100

    chore(model): create dto-object for ingredient entity
    Used to return ingredient objects to a search request.
    
    Refs: #33

---
+++ backend/src/main/java/com/bartsch/costlydiet/model/dto/ingredient/IngredientSearchResDto.java
@@ -0,0 +1,11 @@
+package com.bartsch.costlydiet.model.dto.ingredient;
+
+import lombok.Data;
+
+@Data
+public class IngredientSearchResDto {
+  private Long id;
+  private String name;
+  private Long price;
+  private String notes;
+}
