31bea63e0fa693b632f34ba9a2a40046c6d1aa05
alice <alice@email>
2026-02-12 18:02:32 +0100

    chore(persistance): add custom query for searching ingredients by name
    JpaRepository does not provide a method for this use case, so a custom
    query had to be written.
    
    Refs: #44

--- backend/src/main/java/com/bartsch/costlydiet/repository/IngredientRepository.java
+++ backend/src/main/java/com/bartsch/costlydiet/repository/IngredientRepository.java
@@ -1,8 +1,15 @@
 package com.bartsch.costlydiet.repository;
 
 import com.bartsch.costlydiet.model.entity.Ingredient;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
 
+  @Query("SELECT i FROM Ingredient i WHERE LOWER(i.name) LIKE LOWER(CONCAT('%', :name, '%'))")
+  List<Ingredient> searchByNameContains(@Param("name") String name);
 }
