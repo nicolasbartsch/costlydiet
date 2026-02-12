27e3d53723e6411b4cb5c0973cbd0a0357839e51
bob <bob@email>
2026-02-12 21:27:44 +0100

    feat(model): add create dto mapping to ingredient mapper
    This is necessary for the ingredient creation logic.
    
    Refs: #55

--- backend/src/main/java/com/bartsch/costlydiet/model/mapper/IngredientMapper.java
+++ backend/src/main/java/com/bartsch/costlydiet/model/mapper/IngredientMapper.java
@@ -1,5 +1,7 @@
 package com.bartsch.costlydiet.model.mapper;
 
+import com.bartsch.costlydiet.model.dto.ingredient.IngredientCreateReqDto;
+import com.bartsch.costlydiet.model.dto.ingredient.IngredientCreateResDto;
 import com.bartsch.costlydiet.model.dto.ingredient.IngredientSearchResDto;
 import com.bartsch.costlydiet.model.entity.Ingredient;
 
@@ -10,7 +12,11 @@ import org.mapstruct.Mapper;
 @Mapper(componentModel = "spring")
 public interface IngredientMapper {
 
+  Ingredient toEntity(IngredientCreateReqDto ingredientCreateReqDto);
+
   IngredientSearchResDto toIngredientSearchResDto(Ingredient ingredient);
 
   List<IngredientSearchResDto> toIngredientSearchResDtoList(List<Ingredient> ingredients);
+
+  IngredientCreateResDto toIngredientCreateResDto(Ingredient ingredient);
 }
