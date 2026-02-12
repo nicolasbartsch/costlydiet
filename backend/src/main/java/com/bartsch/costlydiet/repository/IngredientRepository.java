4ae8e0d8f9bd7f6eece5f810bc971b3fa1f1449d
alice <alice@email>
2026-02-12 17:18:39 +0100

    feat(persistance): add repository skeleton for ingredient
    Makes it possible to query ingredient data from the database.
    
    Reviewed by: Bob
    Refs: #18

---
+++ backend/src/main/java/com/bartsch/costlydiet/repository/IngredientRepository.java
@@ -0,0 +1,8 @@
+package com.bartsch.costlydiet.repository;
+
+import com.bartsch.costlydiet.model.entity.Ingredient;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
+
+}
