e6bc7d2a4ff60ddf0bae978c30ae67f3660ecfc0
alice <alice@email>
2026-02-12 18:00:14 +0100

    chore(model): add two new mapping functions to ingredient mapper
    Needed to convert an ingredient entity into a Search Response DTO.
    
    Refs: #35

--- backend/src/main/java/com/bartsch/costlydiet/model/mapper/IngredientMapper.java
+++ backend/src/main/java/com/bartsch/costlydiet/model/mapper/IngredientMapper.java
@@ -1,8 +1,16 @@
 package com.bartsch.costlydiet.model.mapper;
 
+import com.bartsch.costlydiet.model.dto.ingredient.IngredientSearchResDto;
+import com.bartsch.costlydiet.model.entity.Ingredient;
+
+import java.util.List;
+
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface IngredientMapper {
 
+  IngredientSearchResDto toIngredientSearchResDto(Ingredient ingredient);
+
+  List<IngredientSearchResDto> toIngredientSearchResDtoList(List<Ingredient> ingredients);
 }
