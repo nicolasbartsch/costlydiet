e6e0a7bd47e6b62cda78cf47526305876c176717
bob <bob@email>
2026-02-12 23:02:52 +0100

    Display ingredients as a grid in the UI

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { MainLayout } from './core/components/main-layout/main-layout';
+import { IngredientList } from './feature/ingredient-list/ingredient-list';
 
 export const routes: Routes = [
   {
     path: '',
     component: MainLayout,
     children: [
+      { path: 'ingredients', component: IngredientList },
     ]
   }
 ];
---
+++ frontend/src/app/feature/ingredient-list/ingredient-list.ts
@@ -0,0 +1,38 @@
+import { inject, Component, signal } from '@angular/core';
+import { IngredientApi } from '../../core/services/api/ingredient.api';
+import { IngredientSearchResDto } from '../../shared/model/ingredient';
+import { FormsModule } from '@angular/forms';
+import { LucideAngularModule } from 'lucide-angular';
+
+@Component({
+  selector: 'app-ingredient-list',
+  imports: [FormsModule, LucideAngularModule],
+  templateUrl: './ingredient-list.html',
+  styleUrl: './ingredient-list.css',
+})
+export class IngredientList {
+
+  private ingredientApiService = inject(IngredientApi)
+
+  protected ingredients = signal<IngredientSearchResDto[] | null>(null);
+  protected searchQuery = '';
+
+  get filteredIngredients(): IngredientSearchResDto[] {
+    return (this.ingredients() || []).filter(i =>
+      i.name.toLowerCase().includes(this.searchQuery.toLowerCase())
+    );
+  }
+
+  constructor() {
+    this.ingredientApiService.searchIngredients('')
+      .subscribe({
+        next: (ingredients) => {
+          this.ingredients.set(ingredients);
+        },
+        error: (err) => {
+          console.error(err);
+        }
+      })
+  }
+
+}
