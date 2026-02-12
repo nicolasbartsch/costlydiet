320203d67d1abb4da764c515f22ab89d826ce240
bob <bob@email>
2026-02-12 23:05:14 +0100

    (UI): add stuff

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,11 +1,15 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { LucideAngularModule, PiggyBank, Flame, NotebookPen, Plus } from 'lucide-angular';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    importProvidersFrom(
+      LucideAngularModule.pick({ PiggyBank, Flame, NotebookPen, Plus })
+    )
+  ],
 };
