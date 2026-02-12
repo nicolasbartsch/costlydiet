e233389c594e12f4b8bf94496115c68f994a3a2d
bob <bob@email>
2026-02-12 22:17:09 +0100

    (UI): add main layout to the routes config
    Refs: #11

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { MainLayout } from './core/components/main-layout/main-layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayout,
+    children: [
+    ]
+  }
+];
+
