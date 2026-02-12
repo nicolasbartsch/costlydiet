c21b83292614073e9e69fe6de480c15b70ecb208
alice <alice@email>
2026-02-12 18:05:24 +0100

    feat(controller): add ingredient search endpoint
    Utilizes the service method to fetch ingredients by name which is needed
    to later build the UI ingredient list.
    
    Refs: #49

--- backend/src/main/java/com/bartsch/costlydiet/controller/IngredientEndpoint.java
+++ backend/src/main/java/com/bartsch/costlydiet/controller/IngredientEndpoint.java
@@ -1,10 +1,13 @@
 package com.bartsch.costlydiet.controller;
 
+import com.bartsch.costlydiet.model.dto.ingredient.IngredientSearchResDto;
 import com.bartsch.costlydiet.service.IngredientService;
 
-import lombok.extern.slf4j.Slf4j;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -14,6 +17,12 @@ public class IngredientEndpoint {
 
   public IngredientEndpoint(IngredientService ingredientService) {
     this.ingredientService = ingredientService;
+  }
+
+  @GetMapping("/ingredients")
+  public List<IngredientSearchResDto> searchIngredients(@RequestParam(required = false) String name) {
+    log.info("getIngredients()");
+    return ingredientService.searchIngredients(name);
   }
 
 }
