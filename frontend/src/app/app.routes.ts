0d6717ff8125b3dd462efb6ac127ea55eccc95ed
mark <mark@email>
2026-02-12 23:59:24 +0100

    (UI)
    Refs #66

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,14 +1,21 @@
 import { Routes } from '@angular/router';
 import { MainLayout } from './core/components/main-layout/main-layout';
 import { IngredientList } from './feature/ingredient-list/ingredient-list';
+import { IngredientCreate } from './feature/ingredient-create/ingredient-create';
 
 export const routes: Routes = [
   {
     path: '',
     component: MainLayout,
     children: [
-      { path: 'ingredients', component: IngredientList },
-    ]
+      {
+        path: 'ingredients',
+        component: IngredientList
+      },
+      {
+        path: 'ingredients/create',
+        component: IngredientCreate
+      },]
   }
 ];
 
