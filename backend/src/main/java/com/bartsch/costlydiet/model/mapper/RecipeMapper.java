d042df5ffcee5840c1a594045129c5f1946191e4
david <david@email>
2026-02-15 23:54:27 +0100

    fix
    Refs: #49

--- backend/src/main/java/com/bartsch/costlydiet/model/mapper/RecipeMapper.java
+++ backend/src/main/java/com/bartsch/costlydiet/model/mapper/RecipeMapper.java
@@ -1,15 +1,25 @@
 package com.bartsch.costlydiet.model.mapper;
 
 import com.bartsch.costlydiet.model.entity.Recipe;
+import com.bartsch.costlydiet.model.entity.RecipeIngredient;
 import com.bartsch.costlydiet.model.dto.recipe.RecipeSearchResDto;
+import com.bartsch.costlydiet.model.dto.recipeingredient.RecipeIngredientDetailDto;
 
 import java.util.List;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface RecipeMapper {
   RecipeSearchResDto toRecipeSearchResDto(Recipe recipe);
 
   List<RecipeSearchResDto> toRecipeSearchResDtoList(List<Recipe> recipes);
+
+  @Mapping(source = "ingredient.name", target = "name")
+  @Mapping(source = "ingredient.price", target = "price")
+  @Mapping(source = "amount", target = "amount")
+  RecipeIngredientDetailDto toRecipeIngredientDetailDto(RecipeIngredient recipeIngredient);
+
+  List<RecipeIngredientDetailDto> toRecipeIngredientDetailDtoList(List<RecipeIngredient> recipeIngredients);
 }
