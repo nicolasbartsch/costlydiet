0a52e7daef5135861b9b43927e0d32d1b2446828
sarah <sarah@email>
2026-02-15 23:30:09 +0100

    feat: expose business logic to fetch recipe details through the rest api

--- backend/src/main/java/com/bartsch/costlydiet/controller/RecipeEndpoint.java
+++ backend/src/main/java/com/bartsch/costlydiet/controller/RecipeEndpoint.java
@@ -7,7 +7,9 @@ import com.bartsch.costlydiet.service.RecipeService;
 import java.util.List;
 
 import com.bartsch.costlydiet.model.dto.recipe.RecipeSearchResDto;
+import com.bartsch.costlydiet.model.dto.recipe.RecipeDetailDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,5 +29,11 @@ public class RecipeEndpoint {
   public List<RecipeSearchResDto> searchRecipes(@RequestParam(required = false) String name) {
     log.info("searchRecipes()");
     return recipeService.searchRecipes(name);
+  }
+
+  @GetMapping("{id}")
+  public RecipeDetailDto getRecipe(@PathVariable Long id) {
+    log.info("getRecipe({})", id);
+    return recipeService.getRecipe(id);
   }
 }
