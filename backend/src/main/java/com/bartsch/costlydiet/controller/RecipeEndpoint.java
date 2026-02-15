e66b43f7b45d4021a76ea6ad09c194615285a94d
david <david@email>
2026-02-15 23:55:50 +0100

    (controller)

--- backend/src/main/java/com/bartsch/costlydiet/controller/RecipeEndpoint.java
+++ backend/src/main/java/com/bartsch/costlydiet/controller/RecipeEndpoint.java
@@ -7,9 +7,12 @@ import com.bartsch.costlydiet.service.RecipeService;
 import java.util.List;
 
 import com.bartsch.costlydiet.model.dto.recipe.RecipeSearchResDto;
+import com.bartsch.costlydiet.model.dto.recipe.RecipeCreateReqDto;
 import com.bartsch.costlydiet.model.dto.recipe.RecipeDetailDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,5 +38,11 @@ public class RecipeEndpoint {
   public RecipeDetailDto getRecipe(@PathVariable Long id) {
     log.info("getRecipe({})", id);
     return recipeService.getRecipe(id);
+  }
+
+  @PostMapping()
+  public RecipeDetailDto createRecipe(@RequestBody RecipeCreateReqDto recipeCreateReqDto) {
+    log.info("createRecipe({})", recipeCreateReqDto);
+    return recipeService.createRecipe(recipeCreateReqDto);
   }
 }
