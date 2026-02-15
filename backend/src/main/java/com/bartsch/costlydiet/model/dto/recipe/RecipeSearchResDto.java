9d544d9450a5d5d4516034f1153adc9c0cb38ccf
sarah <sarah@email>
2026-02-15 17:57:32 +0100

    feat(model): update
    New recipe search dto which is necessary for the search function to
    work.
    
    Also creates two new mapping functions used to map between recipe
    entities and search dtos.

---
+++ backend/src/main/java/com/bartsch/costlydiet/model/dto/recipe/RecipeSearchResDto.java
@@ -0,0 +1,9 @@
+package com.bartsch.costlydiet.model.dto.recipe;
+
+import lombok.Data;
+
+@Data
+public class RecipeSearchResDto {
+  private Long id;
+  private String name;
+}
--- backend/src/main/java/com/bartsch/costlydiet/model/mapper/RecipeMapper.java
+++ backend/src/main/java/com/bartsch/costlydiet/model/mapper/RecipeMapper.java
@@ -1,8 +1,15 @@
 package com.bartsch.costlydiet.model.mapper;
 
+import com.bartsch.costlydiet.model.entity.Recipe;
+import com.bartsch.costlydiet.model.dto.recipe.RecipeSearchResDto;
+
+import java.util.List;
+
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface RecipeMapper {
+  RecipeSearchResDto toRecipeSearchResDto(Recipe recipe);
 
+  List<RecipeSearchResDto> toRecipeSearchResDtoList(List<Recipe> recipes);
 }
