1d9a58ce36197cd59a0a4206302ed3443a1ae5bd
mark <mark@email>
2026-02-15 14:24:20 +0100

    feat(model)
    Refs: #72

--- backend/src/main/java/com/bartsch/costlydiet/model/mapper/IngredientMapper.java
+++ backend/src/main/java/com/bartsch/costlydiet/model/mapper/IngredientMapper.java
@@ -3,6 +3,8 @@ package com.bartsch.costlydiet.model.mapper;
 import com.bartsch.costlydiet.model.dto.ingredient.IngredientCreateReqDto;
 import com.bartsch.costlydiet.model.dto.ingredient.IngredientCreateResDto;
 import com.bartsch.costlydiet.model.dto.ingredient.IngredientSearchResDto;
+import com.bartsch.costlydiet.model.dto.ingredient.IngredientUpdateReqDto;
+import com.bartsch.costlydiet.model.dto.ingredient.IngredientUpdateResDto;
 import com.bartsch.costlydiet.model.entity.Ingredient;
 
 import java.util.List;
@@ -14,9 +16,15 @@ public interface IngredientMapper {
 
   Ingredient toEntity(IngredientCreateReqDto ingredientCreateReqDto);
 
+  Ingredient toEntity(IngredientUpdateReqDto ingredientUpdateReqDto);
+
   IngredientSearchResDto toIngredientSearchResDto(Ingredient ingredient);
 
   List<IngredientSearchResDto> toIngredientSearchResDtoList(List<Ingredient> ingredients);
 
   IngredientCreateResDto toIngredientCreateResDto(Ingredient ingredient);
+
+  IngredientUpdateResDto toIngredientUpdateResDto(Ingredient ingredient);
+
+  List<IngredientUpdateResDto> toIngredientUpdateResDtoList(List<Ingredient> ingredients);
 }
