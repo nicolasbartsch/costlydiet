edf6aa646f88fde1e976f15682d0cb637cb0b3b4
sarah <sarah@email>
2026-02-15 17:22:54 +0100

    feat: add new data model for recipe object

---
+++ backend/src/main/java/com/bartsch/costlydiet/model/entity/Recipe.java
@@ -0,0 +1,36 @@
+package com.bartsch.costlydiet.model.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Data
+public class Recipe {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column
+  private String name;
+
+  @Column
+  private String instructions;
+
+  @OneToMany(mappedBy = "recipe", cascade = CascadeType.ALL)
+  private List<RecipeIngredient> recipeIngredients;
+
+  @Column
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+}
