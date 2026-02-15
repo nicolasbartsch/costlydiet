0c02c6b9d8e9733343abfaa9011246a281bb2441
mark <mark@email>
2026-02-15 15:03:12 +0100

    feat(service): add business logic to fetch an ingredient by its id

--- backend/src/main/java/com/bartsch/costlydiet/service/IngredientService.java
+++ backend/src/main/java/com/bartsch/costlydiet/service/IngredientService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.bartsch.costlydiet.model.dto.ingredient.IngredientCreateReqDto;
 import com.bartsch.costlydiet.model.dto.ingredient.IngredientCreateResDto;
+import com.bartsch.costlydiet.model.dto.ingredient.IngredientDetailDto;
 import com.bartsch.costlydiet.model.dto.ingredient.IngredientSearchResDto;
 import com.bartsch.costlydiet.model.dto.ingredient.IngredientUpdateReqDto;
 import com.bartsch.costlydiet.model.dto.ingredient.IngredientUpdateResDto;
@@ -11,6 +12,8 @@ import com.bartsch.costlydiet.model.dto.ingredient.IngredientUpdateResDto;
 public interface IngredientService {
 
   List<IngredientSearchResDto> searchIngredients(String name);
+
+  IngredientDetailDto getIngredientById(Long id);
 
   IngredientCreateResDto addIngredient(IngredientCreateReqDto ingredientCreateReqDto);
 
--- backend/src/main/java/com/bartsch/costlydiet/service/implementation/IngredientServiceImpl.java
+++ backend/src/main/java/com/bartsch/costlydiet/service/implementation/IngredientServiceImpl.java
@@ -2,6 +2,7 @@ package com.bartsch.costlydiet.service.implementation;
 
 import com.bartsch.costlydiet.model.dto.ingredient.IngredientCreateReqDto;
 import com.bartsch.costlydiet.model.dto.ingredient.IngredientCreateResDto;
+import com.bartsch.costlydiet.model.dto.ingredient.IngredientDetailDto;
 import com.bartsch.costlydiet.model.dto.ingredient.IngredientSearchResDto;
 import com.bartsch.costlydiet.model.dto.ingredient.IngredientUpdateReqDto;
 import com.bartsch.costlydiet.model.dto.ingredient.IngredientUpdateResDto;
@@ -39,6 +40,14 @@ public class IngredientServiceImpl implements IngredientService {
   }
 
   @Override
+  public IngredientDetailDto getIngredientById(Long id) {
+    Ingredient ingredient = ingredientRepository.findById(id)
+        .orElseThrow(() -> new EntityNotFoundException("Ingredient not found with id: " + id));
+
+    return ingredientMapper.toIngredientDetailDto(ingredient);
+  }
+
+  @Override
   public IngredientCreateResDto addIngredient(IngredientCreateReqDto ingredientCreateReqDto) {
     log.debug("Adding ingredient {}", ingredientCreateReqDto);
     return ingredientMapper
@@ -60,4 +69,5 @@ public class IngredientServiceImpl implements IngredientService {
 
     return ingredientMapper.toIngredientUpdateResDto(ingredient);
   }
+
 }
