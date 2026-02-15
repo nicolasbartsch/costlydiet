5ad44236651f30a0e8939340602f581e4035e889
sarah <sarah@email>
2026-02-15 23:32:32 +0100

    chore(UI): export new interfaces for recipe module
    Refs: #59

--- frontend/src/app/shared/model/recipe.ts
+++ frontend/src/app/shared/model/recipe.ts
@@ -2,3 +2,20 @@ export interface RecipeSearchResDto {
   id: number;
   name: string;
 }
+
+export interface RecipeDetailDto {
+  id: number;
+  name: string;
+  instructions: string;
+  recipeIngredients: RecipeIngredientDetailDto[];
+  priceTotal: number;
+  caloriesTotal: number;
+  amountTotal: number;
+}
+
+export interface RecipeIngredientDetailDto {
+  name: string;
+  amount: number;
+  price: number;
+  calories: number;
+}
