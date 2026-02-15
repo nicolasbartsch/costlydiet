8c17ce3795b0761a902db442a5f665720c6d4e7e
sarah <sarah@email>
2026-02-15 23:25:02 +0100

    chore: fix
    The custom query is necessary to include the recipe ingredients with it,
    so they can be also put into the dto that gets send to the frontend. The
    join makes it more effective than fetching them in a separate call to
    the RecipeIngredient repository
    
    Refs: #68

--- backend/src/main/java/com/bartsch/costlydiet/repository/RecipeRepository.java
+++ backend/src/main/java/com/bartsch/costlydiet/repository/RecipeRepository.java
@@ -3,6 +3,7 @@ package com.bartsch.costlydiet.repository;
 import com.bartsch.costlydiet.model.entity.Recipe;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,4 +12,7 @@ import org.springframework.data.repository.query.Param;
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
   @Query("SELECT r FROM Recipe r WHERE LOWER(r.name) LIKE LOWER(CONCAT('%', :name, '%'))")
   List<Recipe> searchByNameContains(@Param("name") String name);
+
+  @Query("SELECT r FROM Recipe r JOIN FETCH r.recipeIngredients ri JOIN FETCH ri.ingredient WHERE r.id = :id")
+  Optional<Recipe> findByIdWithIngredients(@Param("id") Long id);
 }
