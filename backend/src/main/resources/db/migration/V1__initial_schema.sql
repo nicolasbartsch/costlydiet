f243736b1fb7a6142a5f920edbd13198eccefddc
sarah <sarah@email>
2026-02-15 17:38:40 +0100

    chore(persistance): add 'recipe' and 'recipe ingredient' schema to the database init script
    Refs: #85

--- backend/src/main/resources/db/migration/V1__initial_schema.sql
+++ backend/src/main/resources/db/migration/V1__initial_schema.sql
@@ -1,4 +1,6 @@
 CREATE SEQUENCE ingredient_seq START WITH 1 INCREMENT BY 50;
+CREATE SEQUENCE recipe_ingredient_seq START WITH 1 INCREMENT BY 50;
+CREATE SEQUENCE recipe_seq START WITH 1 INCREMENT BY 50;
 
 -- Tables
 CREATE TABLE ingredient (
@@ -9,3 +11,22 @@ CREATE TABLE ingredient (
     name VARCHAR(255),
     notes TEXT
 );
+
+CREATE TABLE recipe (
+    created_at TIMESTAMP(6),
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    instructions TEXT,
+    name VARCHAR(255)
+);
+
+CREATE TABLE recipe_ingredient (
+       id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+       amount BIGINT,
+       ingredient_id BIGINT,
+       recipe_id BIGINT NOT NULL
+);
+
+-- Foreign Keys
+ALTER TABLE recipe_ingredient
+    ADD CONSTRAINT fk_recipe FOREIGN KEY (recipe_id) REFERENCES recipe(id),
+    ADD CONSTRAINT fk_ingredient FOREIGN KEY (ingredient_id) REFERENCES ingredient(id);
