2dc5607387dbe0d1b0281f201d34ba5797174380
sarah <sarah@email>
2026-02-15 18:02:42 +0100

    implement api endpoint for searching existing recipes by name
    Exposes the existing service and persistance logic for fetching recipes
    throught the backend REST API. Now the functionality can be integrated
    into the frontend.

--- backend/src/main/java/com/bartsch/costlydiet/controller/RecipeEndpoint.java
+++ backend/src/main/java/com/bartsch/costlydiet/controller/RecipeEndpoint.java
@@ -2,7 +2,14 @@ package com.bartsch.costlydiet.controller;
 
 import lombok.extern.slf4j.Slf4j;
 
+import com.bartsch.costlydiet.service.RecipeService;
+
+import java.util.List;
+
+import com.bartsch.costlydiet.model.dto.recipe.RecipeSearchResDto;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,7 +17,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "api/recipes")
 public class RecipeEndpoint {
 
-  public RecipeEndpoint() {
+  private final RecipeService recipeService;
+
+  public RecipeEndpoint(RecipeService recipeService) {
+    this.recipeService = recipeService;
   }
 
+  @GetMapping()
+  public List<RecipeSearchResDto> searchRecipes(@RequestParam(required = false) String name) {
+    log.info("searchRecipes()");
+    return recipeService.searchRecipes(name);
+  }
 }
